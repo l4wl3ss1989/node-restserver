64c9ab304747e040c029d334e207616768456f81
l4wl3ss1989 <l4wl3ss1989@gmail.com>
2018-09-23 12:47:59 +0200

    Rest server - user -bcypt change to bcryptjs fix

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const _= require('underscore');
 const User = require('../models/user');
 const app = express();
